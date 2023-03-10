25fb1e9ed5e0e13a6452bf1f02b68505678b3e98
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-03-10 11:47:55 +0100

    create folder structure

---
+++ src/server.js
@@ -0,0 +1,66 @@
+import Express from "express";
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+import { join } from "path";
+import {
+  genericErrorHandler,
+  badRequestHandler,
+  unauthorizedHandler,
+  notfoundHandler,
+} from "./errorsHandlers.js";
+import createHttpError from "http-errors";
+
+const server = Express();
+const port = process.env.PORT || 3001;
+
+const publicFolderPath = join(process.cwd(), "./public");
+
+console.log(process.env.SECRET);
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(
+    `Request method ${req.method} -- url ${req.url} -- ${new Date()}`
+  );
+  req.user = "Hardika";
+  next();
+};
+
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
+
+server.use(Express.static(publicFolderPath));
+// server.use(cors());
+server.use(
+  cors({
+    origin: (currentOrigin, corsNext) => {
+      if (!currentOrigin || whitelist.indexOf(currentOrigin) !== -1) {
+        // origin is in the whitelist
+        corsNext(null, true);
+      } else {
+        // origin is not in the whitelist
+        corsNext(
+          createHttpError(
+            400,
+            `Origin ${currentOrigin} is not in the whitelist!`
+          )
+        );
+      }
+    },
+  })
+);
+server.use(loggerMiddleware);
+server.use(Express.json());
+
+server.listen(port, () => {
+  console.table(listEndpoints(server));
+  console.log(`Server is running on port ${port}`);
+});
+
+// ************************* ERROR HANDLERS *******************
+server.use(badRequestHandler); // 400
+server.use(unauthorizedHandler); // 401
+server.use(notfoundHandler); // 404
+server.use(genericErrorHandler); // 500 (this should ALWAYS be the last one)
+
+server.on("error", (error) =>
+  console.log(`Server is not running due to: ${error}`)
+);
